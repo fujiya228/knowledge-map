aa6db8fe4ba015ba9f5d13886c66206d193b5f70
fujiya228m <fujiya0xE2@gmail.com>
2020-06-30 00:17:49 +0900

    [add]Edit.vueの作成

---
+++ src/components/organisms/Edit.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="Edit">
+    <!-- 権限あるとき=>エディターを表示 -->
+    <!-- 権限ないとき=>テキストのみ表示 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Edit",
+  created() {}
+};
+</script>
